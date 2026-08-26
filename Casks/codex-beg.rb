ae34d16e61ae11b3c836c3833607b68c4eb49811
newchille <39554889+newchille@users.noreply.github.com>
2026-08-27 02:10:27 +0700

    Update codex-beg cask to 0.1.3

--- Casks/codex-beg.rb
+++ Casks/codex-beg.rb
@@ -1,6 +1,6 @@
 cask "codex-beg" do
-  version "0.1.2"
-  sha256 "48631816c54c40e50961db588b985a3974832e20bb98310fa07f9b4457bf5856"
+  version "0.1.3"
+  sha256 "8be1e3b6393013f66bc87b804b783e09bb901c1d57f096050b95e3ab968349f7"
 
   url "https://github.com/newchille/codex-beg/releases/download/v#{version}/Codex-BEG-#{version}-mac-arm64.dmg"
   name "Codex BEG"
